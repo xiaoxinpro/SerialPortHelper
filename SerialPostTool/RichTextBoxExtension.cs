2d9417d8b259a79d5f041e1595482513df9d467c
Chishin <pro@xxgzs.org>
2019-03-13 19:54:28 +0800

    增加富文本控件

---
+++ SerialPostTool/RichTextBoxExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SerialPostTool
+{
+    public static class RichTextBoxExtension
+    {
+        /// <summary>
+        /// 富文本框扩展类
+        /// </summary>
+        /// <param name="rtBox">RichTextBox</param>
+        /// <param name="text">增加的文本</param>
+        /// <param name="color">颜色</param>
+        /// <param name="addNewLine">是否换行</param>
+        /// <example>RichTextBox1.AppendTextColorful("Your message here",Color.Green);</example>
+        public static void AppendTextColorful(this RichTextBox rtBox, string text, Color color, bool addNewLine = true)
+        {
+            if (addNewLine)
+            {
+                text += Environment.NewLine;
+            }
+            rtBox.SelectionStart = rtBox.TextLength;
+            rtBox.SelectionLength = 0;
+            rtBox.SelectionColor = color;
+            rtBox.AppendText(text);
+            rtBox.SelectionColor = rtBox.ForeColor;
+        }
+    }
+}
