2f8e093573749e8eedd93cd09495ac62fe0c23f3
Chishin <pro@xxgzs.org>
2019-03-16 16:46:50 +0800

    增加串口写入配置类

---
+++ SerialPostTool/SerialWriteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SerialPostTool
+{
+    public class SerialWriteConfig
+    {
+        public SerialWriteConfig()
+        {
+
+        }
+
+        public string Name { get; set; }
+        public string Data { get; set; }
+        public SerialPortHelperLib.SerialFormat Format { get; set; }
+        public bool IsTimer { get; set; }
+        public int Timer { get; set; }
+
+    }
+}
