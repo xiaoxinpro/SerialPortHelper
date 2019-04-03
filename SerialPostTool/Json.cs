3700fbfdc6c171d9b05513a2d401ed1cfe4f2723
Chishin <pro@xxgzs.org>
2019-04-03 21:11:34 +0800

    重构Json类文件

--- SerialPostTool/Json.cs
+++ SerialPostTool/Json.cs
@@ -1,26 +1,19 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Web.Script.Serialization; //需要引用：System.Web.Extensions.dll
+using Newtonsoft.Json;
 
 namespace SerialPostTool
 {
     public static class Json
     {
-        /// <summary>
-        /// 内存对象转换为json字符串
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public static string ToString(object obj)
-        {
-            StringBuilder sb = new StringBuilder();
-            JavaScriptSerializer json = new JavaScriptSerializer();
-            json.Serialize(obj, sb);
-            return sb.ToString();
-        }
         /// <summary>
         /// Json字符串转内存对象
         /// </summary>
@@ -34,6 +27,27 @@ namespace SerialPostTool
         }
 
         /// <summary>
+        /// 从一个对象信息生成Json串
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string ObjectToJson(object obj)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+        }
+
+        /// <summary>
+        /// 从一个Json串生成对象信息
+        /// </summary>
+        /// <param name="jsonString"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static object JsonToObject(string jsonString, object obj)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject(jsonString, obj.GetType());
+        }
+
+        /// <summary>
         /// 写入文件
         /// </summary>
         /// <param name="path">路径</param>
@@ -42,7 +56,7 @@ namespace SerialPostTool
         {
             using (StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8))
             {
-                sw.Write(ToString(obj));
+                sw.Write(ObjectToJson(obj));
             }
         }
 
@@ -68,6 +82,18 @@ namespace SerialPostTool
                 {
                     return json.Deserialize<T>("");
                 }
+            }
+        }
+
+        public static object ReadFile(string path, object obj)
+        {
+            if (!File.Exists(path))
+            {
+                return null;
+            }
+            using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
+            {
+                return JsonToObject(sr.ReadLine().Trim().ToString(), obj);
             }
         }
     }
