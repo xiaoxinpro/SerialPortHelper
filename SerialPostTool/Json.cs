2f413562b4bf3c2be5a7fc62b56aa2ae6698e709
Chishin <pro@xxgzs.org>
2019-04-04 20:31:52 +0800

    简化JSON类

--- SerialPostTool/Json.cs
+++ SerialPostTool/Json.cs
@@ -7,25 +7,12 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Web.Script.Serialization; //需要引用：System.Web.Extensions.dll
 using Newtonsoft.Json;
 
 namespace SerialPostTool
 {
     public static class Json
     {
-        /// <summary>
-        /// Json字符串转内存对象
-        /// </summary>
-        /// <param name="jsonString"></param>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public static T ToObject<T>(string jsonString)
-        {
-            JavaScriptSerializer json = new JavaScriptSerializer();
-            return json.Deserialize<T>(jsonString);
-        }
-
         /// <summary>
         /// 从一个对象信息生成Json串
         /// </summary>
@@ -48,6 +35,24 @@ namespace SerialPostTool
         }
 
         /// <summary>
+        /// 从一个Json串生成对象信息(包含DataTable和List<>集合对象)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="strJson"></param>
+        /// <returns></returns>
+        public static T JsonToObject<T>(string strJson)
+        {
+            T oRes = default(T);
+            try
+            {
+                oRes = JsonConvert.DeserializeObject<T>(strJson);
+            }
+            catch
+            { }
+            return oRes;
+        }
+
+        /// <summary>
         /// 写入文件
         /// </summary>
         /// <param name="path">路径</param>
@@ -64,27 +69,31 @@ namespace SerialPostTool
         /// 读取文件
         /// </summary>
         /// <param name="path">路径</param>
-        /// <param name="dic">数据</param>
         public static T ReadFile<T>(string path)
         {
-            JavaScriptSerializer json = new JavaScriptSerializer();
             if (!File.Exists(path))
             {
-                return json.Deserialize<T>("");
+                return JsonToObject<T>("");
             }
             using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
             {
                 try
                 {
-                    return ToObject<T>(sr.ReadLine().Trim().ToString());
+                    return JsonToObject<T>(sr.ReadLine().Trim().ToString());
                 }
                 catch (Exception)
                 {
-                    return json.Deserialize<T>("");
+                    return JsonToObject<T>("");
                 }
             }
         }
 
+        /// <summary>
+        /// 读取文件
+        /// </summary>
+        /// <param name="path">路径</param>
+        /// <param name="obj">对象原型</param>
+        /// <returns></returns>
         public static object ReadFile(string path, object obj)
         {
             if (!File.Exists(path))
