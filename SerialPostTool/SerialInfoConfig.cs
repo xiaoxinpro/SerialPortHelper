5eb4f1206ab542ec927652dcc8756185177ad297
Chishin <pro@xxgzs.org>
2019-04-12 21:38:07 +0800

    拆分恢复默认配置

--- SerialPostTool/SerialInfoConfig.cs
+++ SerialPostTool/SerialInfoConfig.cs
@@ -26,11 +26,27 @@ namespace SerialPostTool
         /// </summary>
         public void ResetConfig()
         {
+            ResetFontConfig();
+            ResetTimeConfig();
+        }
+
+        /// <summary>
+        /// 复位字体配置
+        /// </summary>
+        public void ResetFontConfig()
+        {
             Font = new Font("宋体", 9);
             Color1Write = Color.Blue;
             Color2Write = Color.Blue;
             Color1Receive = Color.Green;
             Color2Receive = Color.Green;
+        }
+
+        /// <summary>
+        /// 复位时间格式配置
+        /// </summary>
+        public void ResetTimeConfig()
+        {
             TimeFormat = "yyyy-MM-dd hh:mm:ss.fff";
         }
     }
