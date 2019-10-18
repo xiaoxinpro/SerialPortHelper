5de147ec31b79ef7e385f4f58fb055f3fae65af1
Chishin <pro@xxgzs.org>
2019-10-18 21:41:36 +0800

    增加串口配置类文件

---
+++ SerialPostTool/SerialMainConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SerialPostTool
+{
+    public class SerialMainConfig
+    {
+        public static string Path { get; set; }
+
+        public SerialMainConfig()
+        {
+
+        }
+
+        public Boolean IsSaveConfig1;
+        public Boolean IsSaveConfig2;
+        public Boolean IsAutoLink1;
+        public Boolean IsAutoLink2;
+        public Encoder SerialEncode1;
+        public Encoder SerialEncode2;
+    }
+}
