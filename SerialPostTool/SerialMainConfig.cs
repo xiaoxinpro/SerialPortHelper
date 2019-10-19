f1b29bb5fd9d33c63b488418d07af1f6c316e9e6
Chishin <pro@xxgzs.org>
2019-10-19 15:49:40 +0800

    完善获取默认配置功能

--- SerialPostTool/SerialMainConfig.cs
+++ SerialPostTool/SerialMainConfig.cs
@@ -11,14 +11,31 @@ namespace SerialPostTool
 
         public SerialMainConfig()
         {
-
+            ResetConfig();
         }
 
         public Boolean IsSaveConfig1;
         public Boolean IsSaveConfig2;
         public Boolean IsAutoLink1;
         public Boolean IsAutoLink2;
-        public Encoder SerialEncode1;
-        public Encoder SerialEncode2;
+        public int SerialEncode1;
+        public int SerialEncode2;
+        public SerialPortHelperLib.ConfigComType ConfigCom1;
+        public SerialPortHelperLib.ConfigComType ConfigCom2;
+
+        /// <summary>
+        /// 默认配置
+        /// </summary>
+        public void ResetConfig()
+        {
+            IsSaveConfig1 = false;
+            IsSaveConfig2 = false;
+            IsAutoLink1 = false;
+            IsAutoLink2 = false;
+            SerialEncode1 = 0;
+            SerialEncode2 = 0;
+            SerialPortHelperLib.ConfigCom.GetDefaultConfigCom(ref ConfigCom1);
+            SerialPortHelperLib.ConfigCom.GetDefaultConfigCom(ref ConfigCom2);
+        }
     }
 }
