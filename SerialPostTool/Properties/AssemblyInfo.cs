96f4456ad4bc64a6cbec91214b295effe77b2b7c
chishin <pro@xxgzs.org>
2021-07-10 22:22:11 +0800

    增加图标

--- SerialPostTool/Properties/AssemblyInfo.cs
+++ SerialPostTool/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Chishin")]
 [assembly: AssemblyProduct("SerialPostTool")]
-[assembly: AssemblyCopyright("Copyright © Chishin 2019")]
+[assembly: AssemblyCopyright("Copyright © Chishin 2021")]
 [assembly: AssemblyTrademark("Chishin")]
 [assembly: AssemblyCulture("")]
 
